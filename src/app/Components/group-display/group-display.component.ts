6d8e36c48e14cb0e03e4fb264286a68ae4fa7ce8
Henry Leto <hnr.leto11@gmail.com>
2022-06-18 14:49:12 -0400

    format and extend group view w/ util class

--- src/app/Components/group-display/group-display.component.ts
+++ src/app/Components/group-display/group-display.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import { GroupUpApiService } from "../../Services/group-up-api.service";
-import { Destroyable } from "../../Utils/destroyable";
-import { filter, takeUntil } from "rxjs";
-import { Group } from "../../Models/group";
+import { ActivatedRoute, Router } from '@angular/router';
+import { GroupUpApiService } from '../../Services/group-up-api.service';
+import { Destroyable } from '../../Utils/destroyable';
+import { filter, takeUntil } from 'rxjs';
+import { Group } from '../../Models/group';
+import { Utility } from 'src/app/Utils/utility';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-group-display',
   templateUrl: './group-display.component.html',
-  styleUrls: ['./group-display.component.scss']
+  styleUrls: ['./group-display.component.scss'],
 })
-export class GroupDisplayComponent extends Destroyable implements OnInit {
+export class GroupDisplayComponent extends Utility implements OnInit {
   currentGroup: Group;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _groupUpApiService: GroupUpApiService) {
-    super();
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _groupUpApiService: GroupUpApiService,
+    protected override auth: AuthService,
+    protected override router: Router
+  ) {
+    super(router, auth);
   }
 
   ngOnInit(): void {
     const groupId = this._activatedRoute.snapshot.params['id'];
 
-    this._groupUpApiService.getGroup(groupId)
+    this._groupUpApiService
+      .getGroup(groupId)
       .pipe(
-        filter(group => !!group),
+        filter((group) => !!group),
         takeUntil(this.destroy$)
-      ).subscribe(x => {
+      )
+      .subscribe((x) => {
         console.log(x);
-        return this.currentGroup = x;
-    })
+        return (this.currentGroup = x);
+      });
   }
-
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from "./Screens/home/home.component";
-import { PageNotFoundComponent } from "./Components/page-not-found/page-not-found.component";
-import {GroupCreationComponent} from "./Components/group-creation/group-creation.component";
-import { GroupDisplayComponent } from "./Components/group-display/group-display.component";
+import { HomeComponent } from './Screens/home/home.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { GroupCreationComponent } from './Components/group-creation/group-creation.component';
+import { GroupDisplayComponent } from './Components/group-display/group-display.component';
 import { RouteGuardService as RouteGuard } from './Security/route-guard.service';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: HomeComponent },
   { path: 'not-found', component: PageNotFoundComponent },
-  { 
-    path: 'group', component: GroupCreationComponent,
-    canActivate: [RouteGuard]
+  {
+    path: 'group',
+    component: GroupCreationComponent,
+    canActivate: [RouteGuard],
   },
-  { path: 'group/:id', component: GroupDisplayComponent },
-  { path: '**', redirectTo: '/not-found', pathMatch: 'full' }
+  {
+    path: 'group/:id',
+    component: GroupDisplayComponent,
+    canActivate: [RouteGuard],
+  },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(ROUTES)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
