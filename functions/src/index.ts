28e7487786cdbbbc099ae33f8adbe37e050869f3
henry <hnr.leto11@gmail.com>
2022-11-06 08:23:53 -0500

    add functions. ty copilot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,11 +6,29 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-export const addUser = functions.auth.user().onCreate((user: any) => {
-  console.log('User created', user);
-  return admin
-    .firestore()
-    .collection('users')
-    .doc(user.uid)
-    .set(JSON.parse(JSON.stringify(user)));
+// create a firebase function that inserts a user into firestore when they are created
+export const createUser = functions.auth.user().onCreate((user: any) => {
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    displayName: user.displayName,
+    createdAt: admin.firestore.FieldValue.serverTimestamp()
+  });
+});
+
+// create a firebase function that deletes a user from firestore when they are deleted
+export const deleteUser = functions.auth.user().onDelete((user: any) => {
+  return admin.firestore().collection('users').doc(user.uid).delete();
+});
+
+// create a firebase function that is triggered when a user is updated
+export const updateUser = functions.firestore.document('users/{userId}').onUpdate((change: any, context: any) => {
+  const data = change.after.data();
+  const previousData = change.before.data();
+
+  // check to see if the user's display name has changed
+  if (data.displayName !== previousData.displayName) {
+    // update the display name for the user's auth record
+    return admin.auth().updateUser(context.params.userId, { displayName: data.displayName });
+  }
+  return null;
 });
