db20e59c99eb54e5f51343827f9dba655ee348dd
henry <hnr.leto11@gmail.com>
2022-11-06 08:11:30 -0500

    update firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,16 @@
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+export const addUser = functions.auth.user().onCreate((user: any) => {
+  console.log('User created', user);
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .set(JSON.parse(JSON.stringify(user)));
+});
