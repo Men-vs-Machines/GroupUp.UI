55f43179dd9e597bc6321b4dd42926326a75e36b
henry <hnr.leto11@gmail.com>
2022-11-18 08:29:15 -0500

    update template

--- src/app/Components/group-display/group-display.component.ts
+++ src/app/Components/group-display/group-display.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GroupUpApiService } from '../../Services/group-up-api.service';
 import { Destroyable } from '../../Utils/destroyable';
-import { filter, Subject, takeUntil } from 'rxjs';
+import { filter, map, Observable, Subject, takeUntil, tap } from 'rxjs';
 import { Group } from '../../Models/group';
 import { Utility } from 'src/app/Utils/utility';
 import { AuthService } from 'src/app/Services/auth.service';
 import { DataProviderService } from 'src/app/Services/data-provider.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from 'src/app/Models/user';
 
 @Component({
   selector: 'app-group-display',
@@ -15,9 +16,9 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./group-display.component.scss'],
 })
 export class GroupDisplayComponent extends Utility implements OnInit {
-
-  groupSubject = new Subject<Group>();
-
+  group$ = new Observable<Group>();
+  user$ = new Observable<User[]>();
+  
   constructor(
     private _activatedRoute: ActivatedRoute,
     private dataProviderService: DataProviderService,
@@ -30,12 +31,10 @@ export class GroupDisplayComponent extends Utility implements OnInit {
   ngOnInit(): void {
     const groupId = this._activatedRoute.snapshot.params['id'];
 
-    this.dataProviderService
-      .getGroup(groupId)
-      .pipe(
-        filter((group) => !!group),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(this.groupSubject);
+    this.group$ = this.dataProviderService.getGroup(groupId);
+    this.user$ = this.group$.pipe(
+      filter(group => !!group?.users),
+      map(group => group.users)
+    );
   }
 }
