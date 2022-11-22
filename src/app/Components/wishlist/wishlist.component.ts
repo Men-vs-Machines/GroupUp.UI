721171db3bb84b5db79217aee0bfc2741325b6e0
henry <hnr.leto11@gmail.com>
2022-11-21 20:28:16 -0500

    updat wishlist form

--- src/app/Components/wishlist/wishlist.component.ts
+++ src/app/Components/wishlist/wishlist.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 import { User } from 'src/app/Models/user';
 import { AuthService } from 'src/app/Services/auth.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-wishlist',
@@ -11,28 +12,35 @@ import { AuthService } from 'src/app/Services/auth.service';
 })
 export class WishlistComponent implements OnInit {
   user$: Observable<User>;
-  wishListForm = this.fb.group({
-    items: this.fb.array([])
-  });
+  wishListForm: FormGroup;
 
   get items() {
-    return this.wishListForm.get('items');
+    return this.wishListForm.controls['items'] as FormArray;
   }
 
   constructor(private authService: AuthService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.user$ = this.authService.user$;
-
-    this.patch();
+    this.user$.pipe(
+      map(({wishList}) => wishList.map(item => this.fb.control({value: item, disabled: true})))
+    )
+    .subscribe(data => {
+      this.wishListForm = this.fb.group({
+        items: this.fb.array(data)
+      })
+    });
   }
 
-  patch() {
-    this.user$.subscribe(user => {
-      console.log(user);
-      this.wishListForm.patchValue({
-        items: user.wishList
-      });
-    });
+  addWishListItem() {
+    this.items.push(this.fb.control(''));
+  }
+
+  enableForm(index) {
+    this.items.controls[index].enable();
+  }
+
+  disableForm(index) {
+    this.items.controls[index].disable();
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PageNotFoundComponent } from './Components/page-not-found/page-not-foun
 import { GroupCreationComponent } from './Components/group-creation/group-creation.component';
 import { GroupDisplayComponent } from './Components/group-display/group-display.component';
 import { RouteGuardService as RouteGuard } from './Security/route-guard.service';
+import { UserPageComponent } from './Screens/user-page/user-page.component';
 
 const ROUTES: Routes = [
   {
@@ -16,7 +17,12 @@ const ROUTES: Routes = [
     path: 'group',
     component: GroupCreationComponent,
     canActivate: [RouteGuard],
-  },  
+  },
+  {
+    path: 'user',
+    component: UserPageComponent,
+    canActivate: [RouteGuard],
+  },
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: HomeComponent },
   { path: 'not-found', component: PageNotFoundComponent },
