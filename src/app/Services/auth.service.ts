a13a0ae6e95abba1418714884d6d257e80996f7b
Henry <hnr.leto11@gmail.com>
2023-03-27 18:07:33 -0400

    update displays

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -2,27 +2,21 @@ import { mapUserToEmailSignIn } from './../Utils/user-dto';
 import { Injectable } from '@angular/core';
 import {
   BehaviorSubject,
-  concatMap,
   delay,
-  EMPTY,
   first,
   forkJoin,
-  iif,
-  mergeMap,
   Observable,
   of,
-  Subject,
   switchMap,
   tap,
   take,
   from,
   map,
-  shareReplay,
-  repeatWhen,
   retryWhen,
   defer,
+  catchError,
+  throwError,
 } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import Firebase from 'firebase/compat';
 import firebaseUser = Firebase.User;
@@ -30,6 +24,7 @@ import { User } from '../Models/user';
 import { DataProviderService } from 'src/app/Services/data-provider.service';
 import { UserService } from './user.service';
 import { SpinnerService } from './spinner.service';
+import { SnackbarService } from './snackbar.service';
 
 @Injectable({
   providedIn: 'root',
@@ -44,7 +39,8 @@ export class AuthService {
     private angularAuth: AngularFireAuth,
     private dataProviderService: DataProviderService,
     private userService: UserService,
-    private spinnerService: SpinnerService
+    private spinnerService: SpinnerService,
+    private snackbarService: SnackbarService
   ) {
     this.angularAuth.authState
       .pipe(
@@ -84,10 +80,7 @@ export class AuthService {
     const newUser = mapUserToEmailSignIn(user);
 
     return from(
-      this.angularAuth.signInWithEmailAndPassword(
-        newUser.email,
-        user.password
-      )
+      this.angularAuth.signInWithEmailAndPassword(newUser.email, user.password)
     ).pipe(
       map((userCredential) => ({ ...newUser, id: userCredential.user.uid })),
       switchMap((user) => this.dataProviderService.getUser(user.id))
@@ -134,7 +127,27 @@ export class AuthService {
     return forkJoin({
       user: this.userService.getUser(firebaseUser.uid).pipe(
         // To keep auth state in sync, we need to make sure that the user is created in the backend
-        retryWhen((errors) => errors.pipe(delay(500), take(3)))
+        retryWhen((err) => {
+          let retries = 1;
+          return err.pipe(
+            delay(500),
+            map((error) => {
+              if (retries++ === 3) {
+                throw error;
+              }
+              return error;
+            })
+          );
+        }),
+        catchError((error) => {
+          this.snackbarService.open('Unable to sign in', 'close', {
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            duration: 2500,
+          });
+
+          return error;
+        })
       ),
       token: from(firebaseUser.getIdToken()),
     }).pipe(
@@ -143,6 +156,10 @@ export class AuthService {
         this._token$.next(token);
         localStorage.setItem('jwt', token);
         this.spinnerService.stop();
+      }),
+      catchError(() => {
+        this.spinnerService.stop();
+        return of(null);
       })
     );
   }
